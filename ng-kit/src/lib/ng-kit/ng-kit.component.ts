7da03e99d16d8186f62739156007dc8448d339bd
Johan <johanschobben@gmail.com>
2024-08-05 20:39:15 +0200

    initial commit

---
+++ ng-kit/src/lib/ng-kit/ng-kit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ng-kit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ng-kit.component.html',
+  styleUrl: './ng-kit.component.css',
+})
+export class NgKitComponent {}
